c616e0a4769a9b1a77b2ec67586a7390df14b236
nimeullaha71 <nimeullahacse@gmail.com>
2024-05-22 02:30:59 +0600

    contact update

--- js/script.js
+++
@@ -1,7 +0,0 @@
-// Analysis
-function showAnalysisSetting(){
-    $('#analysis-setting').css("display", "flex");
-}
-function hideAnalysisSetting(){
-    $('#analysis-setting').css("display", "none");
-}
