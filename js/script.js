1d2816dc3d44b8f652b5c702bd85b9868091deff
nimeullaha71 <nimeullahacse@gmail.com>
2024-05-21 22:11:08 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Analysis
+function showAnalysisSetting(){
+    $('#analysis-setting').css("display", "flex");
+}
+function hideAnalysisSetting(){
+    $('#analysis-setting').css("display", "none");
+}
